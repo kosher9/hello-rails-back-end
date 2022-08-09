bd352c7ff699af8f41bf7fec49b1e8110e6ec804
Cédric Kossi <lionel.kossi@gmail.com>
2022-08-09 19:44:58 +0000

    Improve message sent

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -2,7 +2,7 @@ module V1
   class GreetingsController < ApplicationController
     def index
       greetings = Greeting.all
-      render json: greetings[rand(5)]
+      render json: greetings[rand(greetings.length - 1)]
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     get 'greeting', to: 'greetings#index'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '*page', to: 'static#index', constraints: lambda { |req|
+    !req.xhr? && req.format.html?
+  }
   # Defines the root path route ("/")
   root "static#index"
 end
